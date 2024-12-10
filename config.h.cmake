fa4d6fe406d1d128360032922aad34b352d1b828
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2024-12-09 21:43:36 -0500

    Test the HAVE_ variables actually in the code.  Not testing things like string.h or unistd.h, since the mg code doesn't conditionalize them.

---
+++ config.h.cmake
@@ -0,0 +1,16 @@
+#define WITHOUT_CURSES 1
+#define PACKAGE_NAME "mg"
+#define PACKAGE_STRING "mg 3.7"
+
+#cmakedefine HAVE_FPARSELN 1
+#cmakedefine HAVE_FUTIMENS 1
+#cmakedefine HAVE_LOGIN_TTY 1
+#cmakedefine HAVE_OPENPTY 1
+#cmakedefine HAVE_REALLOCARRAY 1
+#cmakedefine HAVE_STRLCAT 1
+#cmakedefine HAVE_STRLCPY 1
+#cmakedefine HAVE_STRTONUM 1
+
+#cmakedefine HAVE_PTY_H
+#cmakedefine HAVE_UTMP_H
+
